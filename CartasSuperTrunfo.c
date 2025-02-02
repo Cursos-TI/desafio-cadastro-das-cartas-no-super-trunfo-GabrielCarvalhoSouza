007396357414588e9df8cf9b729d83e78c87fb07
Gabriel Carvalho Souza <129614621+GabrielCarvalhoSouza@users.noreply.github.com>
2025-02-02 11:17:45 -0300

    Adcionado 4 cidades

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,131 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+int main() { 
+    // Estado 01
+    // Variáveis da cidade 01 
+    char city01_code[4];
+    char city01_name[50];
+    int city01_population;
+    float city01_area;
+    float city01_pib;
+    int city01_tourism;
 
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    // Coletando informações da cidade 01
+    printf("Digite o código da cidade 01: ");
+    scanf("%s", city01_code);
+    printf("Digite o nome da cidade 01: ");
+    scanf("%s", city01_name);
+    printf("Digite a população da cidade 01: ");
+    scanf("%d", &city01_population);
+    printf("Digite a área da cidade 01: ");
+    scanf("%f", &city01_area);
+    printf("Digite o PIB da cidade 01: ");
+    scanf("%f", &city01_pib);
+    printf("Digite o número de pontos turísticos da cidade 01: ");
+    scanf(" %d", &city01_tourism);
+
+    // Mostrando informações da cidade 01
+    printf("\nCidade 01\n");
+    printf("Código: %s\n", city01_code);
+    printf("Nome: %s\n", city01_name);
+    printf("População: %d\n", city01_population);
+    printf("Área: %.2f\n", city01_area);
+    printf("PIB: %.2f\n", city01_pib);
+    printf("Pontos turísticos: %d\n", city01_tourism);
+
+    // Variáveis da cidade 02
+    char city02_code[4];
+    char city02_name[50];
+    int city02_population;
+    float city02_area;
+    float city02_pib;
+    int city02_tourism;
+
+    // Coletando informações da cidade 02
+    printf("\nDigite o código da cidade 02: ");
+    scanf("%s", city02_code);
+    printf("Digite o nome da cidade 02: ");
+    scanf("%s", city02_name);
+    printf("Digite a população da cidade 02: ");
+    scanf("%d", &city02_population);
+    printf("Digite a área da cidade 02: ");
+    scanf("%f", &city02_area);
+    printf("Digite o PIB da cidade 02: ");
+    scanf("%f", &city02_pib);
+    printf("Digite o número de pontos turísticos da cidade 02: ");
+    scanf(" %d", &city02_tourism);
+
+    // Mostrando informações da cidade 02
+    printf("\nCidade 02\n");
+    printf("Código: %s\n", city02_code);
+    printf("Nome: %s\n", city02_name);
+    printf("População: %d\n", city02_population);
+    printf("Área: %.2f\n", city02_area);
+    printf("PIB: %.2f\n", city02_pib);
+    printf("Pontos turísticos: %d\n", city02_tourism);
+
+    // Variáveis da cidade 03
+    char city03_code[4];    
+    char city03_name[50];
+    int city03_population;
+    float city03_area;
+    float city03_pib;
+    int city03_tourism;
+
+    // Coletando informações da cidade 03
+    printf("\nDigite o código da cidade 03: ");
+    scanf("%s", city03_code);
+    printf("Digite o nome da cidade 03: ");
+    scanf("%s", city03_name);
+    printf("Digite a população da cidade 03: ");
+    scanf("%d", &city03_population);
+    printf("Digite a área da cidade 03: ");
+    scanf("%f", &city03_area);
+    printf("Digite o PIB da cidade 03: ");
+    scanf("%f", &city03_pib);
+    printf("Digite o número de pontos turísticos da cidade 03: ");
+    scanf(" %d", &city03_tourism);
+
+    // Mostrando informações da cidade 03
+    printf("\nCidade 03\n");
+    printf("Código: %s\n", city03_code);
+    printf("Nome: %s\n", city03_name);
+    printf("População: %d\n", city03_population);
+    printf("Área: %.2f\n", city03_area);
+    printf("PIB: %.2f\n", city03_pib);
+    printf("Pontos turísticos: %d\n", city03_tourism);
+
+    // Variáveis da cidade 04
+    char city04_code[4];
+    char city04_name[50];
+    int city04_population;
+    float city04_area;
+    float city04_pib;
+    int city04_tourism;
+
+    // Coletando informações da cidade 04
+    printf("\nDigite o código da cidade 04: ");
+    scanf("%s", city04_code);
+    printf("Digite o nome da cidade 04: ");
+    scanf("%s", city04_name);
+    printf("Digite a população da cidade 04: ");
+    scanf("%d", &city04_population);
+    printf("Digite a área da cidade 04: ");
+    scanf("%f", &city04_area);
+    printf("Digite o PIB da cidade 04");
+    scanf("%f", &city04_pib);
+    printf("Digite o número de pontos turísticos da cidade 04: ");
+    scanf(" %d", &city04_tourism);
+
+    // Mostrando informações da cidade 04
+    printf("\nCidade 04\n");
+    printf("Código: %s\n", city04_code);
+    printf("Nome: %s\n", city04_name);
+    printf("População: %d\n", city04_population);
+    printf("Área: %.2f\n", city04_area);
+    printf("PIB: %.2f\n", city04_pib);
+    printf("Pontos turísticos: %d\n", city04_tourism);
 
     return 0;
 }
+ 
