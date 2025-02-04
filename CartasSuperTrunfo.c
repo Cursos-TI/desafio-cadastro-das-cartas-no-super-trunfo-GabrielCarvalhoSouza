d97f3daf36815734fb7de8249a42b9313884b816
Gabriel Carvalho Souza <129614621+GabrielCarvalhoSouza@users.noreply.github.com>
2025-02-04 15:05:16 -0300

    Incluir pib per capita e densidade populacional

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,28 +1,38 @@
 #include <stdio.h>
 
 int main() { 
-    // Estado 01
-    // Variáveis da cidade 01 
+    // Estado A
+    // Variaveis da cidade 01 
     char city01_code[4];
     char city01_name[50];
     int city01_population;
     float city01_area;
     float city01_pib;
     int city01_tourism;
+    float city01_density;
+    float city01_pib_per_capita;
 
-    // Coletando informações da cidade 01
+    // Coletando informações da cidade 01
     printf("Digite o código da cidade 01: ");
     scanf("%s", city01_code);
+
     printf("Digite o nome da cidade 01: ");
     scanf("%s", city01_name);
+
     printf("Digite a população da cidade 01: ");
     scanf("%d", &city01_population);
+
     printf("Digite a área da cidade 01: ");
     scanf("%f", &city01_area);
+
     printf("Digite o PIB da cidade 01: ");
     scanf("%f", &city01_pib);
+
     printf("Digite o número de pontos turísticos da cidade 01: ");
     scanf(" %d", &city01_tourism);
+
+    city01_density = city01_population / city01_area;
+    city01_pib_per_capita = city01_pib / city01_population;
 
     // Mostrando informações da cidade 01
     printf("\nCidade 01\n");
@@ -32,6 +42,8 @@ int main() {
     printf("Área: %.2f\n", city01_area);
     printf("PIB: %.2f\n", city01_pib);
     printf("Pontos turísticos: %d\n", city01_tourism);
+    printf("Densidade: %.2f\n", city01_density);
+    printf("PIB per capita: %.2f\n", city01_pib_per_capita);
 
     // Variáveis da cidade 02
     char city02_code[4];
@@ -40,20 +52,30 @@ int main() {
     float city02_area;
     float city02_pib;
     int city02_tourism;
+    float city02_density;
+    float city02_pib_per_capita;
 
     // Coletando informações da cidade 02
     printf("\nDigite o código da cidade 02: ");
     scanf("%s", city02_code);
+
     printf("Digite o nome da cidade 02: ");
     scanf("%s", city02_name);
+
     printf("Digite a população da cidade 02: ");
     scanf("%d", &city02_population);
+
     printf("Digite a área da cidade 02: ");
     scanf("%f", &city02_area);
+
     printf("Digite o PIB da cidade 02: ");
     scanf("%f", &city02_pib);
+
     printf("Digite o número de pontos turísticos da cidade 02: ");
     scanf(" %d", &city02_tourism);
+    
+    city02_density = city02_population / city02_area;
+    city02_pib_per_capita = city02_pib / city02_population;
 
     // Mostrando informações da cidade 02
     printf("\nCidade 02\n");
@@ -63,6 +85,8 @@ int main() {
     printf("Área: %.2f\n", city02_area);
     printf("PIB: %.2f\n", city02_pib);
     printf("Pontos turísticos: %d\n", city02_tourism);
+    printf("Densidade: %.2f\n", city02_density);
+    printf("PIB per capita: %.2f\n", city02_pib_per_capita);
 
     // Variáveis da cidade 03
     char city03_code[4];    
@@ -71,20 +95,30 @@ int main() {
     float city03_area;
     float city03_pib;
     int city03_tourism;
+    float city03_density;
+    float city03_pib_per_capita;
 
     // Coletando informações da cidade 03
     printf("\nDigite o código da cidade 03: ");
     scanf("%s", city03_code);
+
     printf("Digite o nome da cidade 03: ");
     scanf("%s", city03_name);
+
     printf("Digite a população da cidade 03: ");
     scanf("%d", &city03_population);
+
     printf("Digite a área da cidade 03: ");
     scanf("%f", &city03_area);
+
     printf("Digite o PIB da cidade 03: ");
     scanf("%f", &city03_pib);
+
     printf("Digite o número de pontos turísticos da cidade 03: ");
     scanf(" %d", &city03_tourism);
+
+    city03_density = city03_population / city03_area;
+    city03_pib_per_capita = city03_pib / city03_population;
 
     // Mostrando informações da cidade 03
     printf("\nCidade 03\n");
@@ -94,6 +128,8 @@ int main() {
     printf("Área: %.2f\n", city03_area);
     printf("PIB: %.2f\n", city03_pib);
     printf("Pontos turísticos: %d\n", city03_tourism);
+    printf("Densidade: %.2f\n", city03_density);
+    printf("PIB per capita: %.2f\n", city03_pib_per_capita);
 
     // Variáveis da cidade 04
     char city04_code[4];
@@ -102,20 +138,31 @@ int main() {
     float city04_area;
     float city04_pib;
     int city04_tourism;
+    float city04_density;
+    float city04_pib_per_capita;
 
     // Coletando informações da cidade 04
     printf("\nDigite o código da cidade 04: ");
     scanf("%s", city04_code);
+
     printf("Digite o nome da cidade 04: ");
     scanf("%s", city04_name);
+
     printf("Digite a população da cidade 04: ");
     scanf("%d", &city04_population);
+
     printf("Digite a área da cidade 04: ");
     scanf("%f", &city04_area);
+
     printf("Digite o PIB da cidade 04");
     scanf("%f", &city04_pib);
+
     printf("Digite o número de pontos turísticos da cidade 04: ");
     scanf(" %d", &city04_tourism);
+
+    city04_density = city04_population / city04_area;
+    city04_pib_per_capita = city04_pib / city04_population;
+
 
     // Mostrando informações da cidade 04
     printf("\nCidade 04\n");
@@ -125,6 +172,8 @@ int main() {
     printf("Área: %.2f\n", city04_area);
     printf("PIB: %.2f\n", city04_pib);
     printf("Pontos turísticos: %d\n", city04_tourism);
+    printf("Densidade: %.2f\n", city04_density);
+    printf("PIB per capita: %.2f\n", city04_pib_per_capita);
 
     return 0;
 }
